f79a05bfbc4498504b701418312d483173a57a81
unknown <godsonemmanuel07@gmail.com>
2021-08-23 12:07:40 +0530

    Fetch data from firebase to use in reducer and created a High order component for using it while fetching the data asynchronously

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -43,7 +43,7 @@ export const addCollectionAndDocuments = async (
   collectionKey,
   objectsToAdd
 ) => {
-  // To add data to forebase
+  // To add data to firebase programmatically
   const collectionRef = firestore.collection(collectionKey);
 
   const batch = firestore.batch();
@@ -53,6 +53,24 @@ export const addCollectionAndDocuments = async (
   });
 
   return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: docSnapshot.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
