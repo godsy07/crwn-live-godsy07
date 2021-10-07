a0bca326b2dd67f5d1aa623d5384740db8b48ba7
unknown <godsonemmanuel07@gmail.com>
2021-10-07 12:07:13 +0530

    Added redux thunk package and implemented it in shop component

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,5 +1,36 @@
 import ShopActionTypes from "./shop.types";
-export const updateCollections = (collectionMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsStuccess = (collectionMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionMap,
 });
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionsRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    collectionsRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        // updateCollections(collectionsMap);
+        dispatch(fetchCollectionsStuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
