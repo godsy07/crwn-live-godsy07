3c5fe7bdf7b0693ade7396eeb5945eec2aa43dea
Godson <godson@incrediblevisibility.com>
2025-06-14 11:09:47 +0530

    fixed inverted attribute being passed on button

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -33,28 +33,27 @@ const googleSignInStyles = css`
     }
 `
 
-const getButtonStyles = props => {
-    if (props.isGoogleSignIn) {
-        return googleSignInStyles;
-    }
+const getButtonStyles = ({ isGoogleSignIn, inverted }) => {
+  if (isGoogleSignIn) return googleSignInStyles;
+  return inverted ? invertedButtonStyles : buttonStyles;
+};
 
-    return props.inverted ? invertedButtonStyles : buttonStyles;
-}
+export const CustomButtonContainer = styled.button.withConfig({
+  shouldForwardProp: (prop) => !['isGoogleSignIn', 'inverted'].includes(prop),
+})`
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  font-size: 15px;
+  text-transform: uppercase;
+  font-family: 'Open Sans Condensed';
+  font-weight: bolder;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
 
-export const  CustomButtonContainer = styled.button`
-    min-width: 165px;
-    width: auto;
-    height: 50px;
-    letter-spacing: 0.5px;
-    line-height: 50px;
-    padding: 0 35px 0 35px;
-    font-size: 15px;
-    text-transform: uppercase;
-    font-family: 'Open Sans Condensed';
-    font-weight: bolder;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-
-    ${getButtonStyles}
-`
+  ${getButtonStyles}
+`;
