cd831b65dc5b1d5fd013c6044647ab40ce29b7a3
Godson <godson@incrediblevisibility.com>
2025-06-14 19:48:36 +0530

    updated Homepage to be responsive

--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
 export const HomePageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px 80px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 80px;
+
+  @media screen and (max-width: 768px) {
+    padding: 20px 40px;
+  }
+
+  @media screen and (max-width: 480px) {
+    padding: 10px 20px;
+  }
+`;
