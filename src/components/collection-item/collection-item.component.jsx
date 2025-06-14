39098855b1472f29272f32a1a5f48eeccdfe7de0
Godson <godson@incrediblevisibility.com>
2025-06-14 11:25:22 +0530

    made collections page more responsive

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import { addItem } from "../../redux/cart/cart.actions";
 
-import { connect } from "react-redux";
-
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({ item }) => {
+  const dispatch = useDispatch();
   const { name, price, imageUrl } = item;
+
   return (
     <div className='collection-item'>
       <div
         className='image'
         style={{
-          background: `url(${imageUrl})`,
+          backgroundImage: `url(${imageUrl})`,
         }}
       />
       <div className='collection-footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton onClick={() => dispatch(addItem(item))} inverted={true} fullWidth={true}>
         Add to Cart
       </CustomButton>
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem(item)),
-});
+export default CollectionItem;
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,55 +1,63 @@
 import styled, { css } from "styled-components";
 
 const buttonStyles = css`
-    background-color: black;
-    color: white;
-    border: none;
+  background-color: black;
+  color: white;
+  border: none;
 
-    &:hover {
-        background-color: white;
-        color: black;
-        border: 1px solid black;
-    }
-`
-
-const invertedButtonStyles = css`
+  &:hover {
     background-color: white;
     color: black;
     border: 1px solid black;
+  }
+`;
 
-    &:hover {
-        background-color: black;
-        color: white;
-        border: none;
-    }
-`
-const googleSignInStyles = css`
-    background-color: #4285f4;
+const invertedButtonStyles = css`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+
+  &:hover {
+    background-color: black;
     color: white;
-    
-    &:hover {
-        background-color: #357ae8;
-        border: none;
-    }
-`
+    border: none;
+  }
+`;
+const googleSignInStyles = css`
+  background-color: #4285f4;
+  color: white;
 
-const getButtonStyles = ({ isGoogleSignIn, inverted }) => {
-  if (isGoogleSignIn) return googleSignInStyles;
-  return inverted ? invertedButtonStyles : buttonStyles;
-};
+  &:hover {
+    background-color: #357ae8;
+    border: none;
+  }
+`;
+
+const getButtonStyles = ({ isGoogleSignIn, inverted, fullWidth }) => css`
+  ${isGoogleSignIn
+    ? googleSignInStyles
+    : inverted
+    ? invertedButtonStyles
+    : buttonStyles};
+
+  ${fullWidth &&
+  css`
+    width: 100%;
+  `}
+`;
 
 export const CustomButtonContainer = styled.button.withConfig({
-  shouldForwardProp: (prop) => !['isGoogleSignIn', 'inverted'].includes(prop),
+  shouldForwardProp: (prop) =>
+    !["isGoogleSignIn", "inverted", "fullWidth"].includes(prop),
 })`
   min-width: 165px;
-  width: auto;
   height: 50px;
   letter-spacing: 0.5px;
   line-height: 50px;
   padding: 0 35px 0 35px;
   font-size: 15px;
   text-transform: uppercase;
-  font-family: 'Open Sans Condensed';
+  font-family: "Open Sans Condensed";
   font-weight: bolder;
   cursor: pointer;
   display: flex;
